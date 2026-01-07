d41dcbc9665ee2c91a40e23076decdd574111daa
awanishkumar <awanishk671@gmail.com>
2026-01-07 13:23:12 +0530

    added home ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,13 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Welcome'),
+      ),
+    );
   }
 }
